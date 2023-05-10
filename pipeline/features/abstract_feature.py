f251bd67b5f10ca362240f94ad08a3bcd3a80137
Artun Boz <artunboz@outlook.com>
2023-05-10 15:39:42 +0200

    Implement interface for features

---
+++ pipeline/features/abstract_feature.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class AbstractFeature(ABC):
+    @abstractmethod
+    def compute_features_image(self, image: np.ndarray) -> np.ndarray:
+        """Computes features for the given image.
+
+        :param image: A numpy ndarray representing the image.
+        :return: A numpy ndarray containing the computed features.
+        """
+
+    @abstractmethod
+    def compute_features_images(self, image_folder_path: str) -> np.ndarray:
+        """Computes features for all the images found in the folder located at the given
+        path and return them in a 2-d numpy array of shape (n_images, n_features).
+
+        :param image_folder_path: A string indicating the path to the folder containing
+            the images.
+        :return: A 2-d numpy array of shape (n_images, n_features).
+        """
