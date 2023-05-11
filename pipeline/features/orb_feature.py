9e9205933011f5746df65c869ad87e399560d096
Artun Boz <artunboz@outlook.com>
2023-05-11 15:20:28 +0200

    Add class doc for orb

--- pipeline/features/orb_feature.py
+++ pipeline/features/orb_feature.py
@@ -12,6 +12,16 @@ class ORBFeature(AbstractFeature):
     def __init__(
         self, resize_size: tuple[int, int], n_keypoints: int, patch_size: int
     ) -> None:
+        """Inits a ORBFeature instance. The underlying implementation relies on OpenCV's
+        implementation of the ORB feature.
+
+        :param resize_size: A 2-tuple of integers indicating the pixel width and height
+            of the resized image.
+        :param n_keypoints: An integer indicating the maximum number of keypoints to
+            detect in an image.
+        :param patch_size: An integer indicating the value to use for the edgeThreshold
+            and patchSize parameters of OpenCV's ORB implementation.
+        """
         self.resize_size: tuple[int, int] = resize_size
         self.orb: cv.ORB = cv.ORB_create(
             nfeatures=n_keypoints, edgeThreshold=patch_size, patchSize=patch_size
@@ -24,6 +34,17 @@ class ORBFeature(AbstractFeature):
         min_samples: int = 20,
         norm: str = "L2",
     ) -> np.ndarray:
+        """Computes ORB features for all the images found in the folder located at the
+        given path and return them in a 2-d numpy array of shape (n_images, n_features).
+
+        :param image_folder_path: A string indicating the path to the folder containing
+            the images.
+        :param eps: A float indicating the eps parameter for DBSCAN.
+        :param min_samples: An integer indicating the min_samples parameter for DBSCAN.
+        :param norm: A string indicating the normalization technique to use when
+            normalizing bag-of-feature histograms to get the final feature vectors.
+        :return: A 2-d numpy array of shape (n_images, n_features).
+        """
         sorted_image_names: list[str] = sorted(os.listdir(image_folder_path))
         descriptors_dict: dict[str, np.ndarray] = {}
         for image_name in tqdm(
@@ -54,7 +75,7 @@ class ORBFeature(AbstractFeature):
         return features
 
     def _get_orb_descriptors(self, image: np.ndarray) -> np.ndarray:
-        """IMAGE HAS TO BE GRAYSCALE"""
+        """The given image must be grayscale."""
         resized_image = cv.resize(image, self.resize_size)
         _, des = self.orb.detectAndCompute(resized_image, None)
         return des
