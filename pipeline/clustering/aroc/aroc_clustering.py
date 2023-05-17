828e6bb59fa434ceb9d18a2928306dac55e86881
Artun Boz <artunboz@outlook.com>
2023-05-17 15:42:47 +0200

    Implement clustering methods

---
+++ pipeline/clustering/aroc/aroc_clustering.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from pipeline.clustering.abstract_clustering import AbstractClustering
+from pipeline.clustering.aroc.aroc import aroc
+
+
+class AROClustering(AbstractClustering):
+    def __init__(self, n_neighbours: int, threshold: float, num_proc: int = 8):
+        self.n_neighbours: int = n_neighbours
+        self.threshold: float = threshold
+        self.num_proc: int = num_proc
+
+    def cluster(self, samples: np.ndarray) -> np.ndarray:
+        return aroc(samples, self.n_neighbours, self.threshold, self.num_proc)
