49140eefe739f2163222ca963086dca4b6aa6d74
Artun Boz <artunboz@outlook.com>
2023-05-18 20:22:59 +0200

    Extract epoch and batch size as params

--- pipeline/dimensionality_reduction/autoencoder_reducer.py
+++ pipeline/dimensionality_reduction/autoencoder_reducer.py
@@ -23,11 +23,15 @@ class AutoencoderReducer(AbstractReducer):
 
         self.standard_scaler: MinMaxScaler = MinMaxScaler()
 
-    def reduce_dimensions(self, samples: np.ndarray) -> np.ndarray:
+    def reduce_dimensions(
+        self, samples: np.ndarray, epochs: int = 10, batch_size: int = 32
+    ) -> np.ndarray:
         """Reduces the dimensions of the given samples.
 
         :param samples: A 2-d numpy array of shape (n_samples, n_features) containing
             the samples.
+        :param epochs: An integer indicating the number of epochs.
+        :param batch_size: A float indicating the batch size to use.
         :return: A 2-d numpy array of shape (n_samples, n_reduced_features) containing
             the samples in a latent space with a lower dimensionality.
         """
@@ -38,8 +42,8 @@ class AutoencoderReducer(AbstractReducer):
         self.autoencoder.fit(
             scaled_train,
             scaled_train,
-            epochs=10,
-            batch_size=32,
+            epochs=epochs,
+            batch_size=batch_size,
             validation_data=(scaled_test, scaled_test),
         )
 
