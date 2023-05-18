760229a11f803e74d558a4451eaa884777bac5fd
Artun Boz <artunboz@outlook.com>
2023-05-18 14:35:02 +0200

    Add sift feature

--- pipeline/features/local_features/orb_feature.py
+++ pipeline/features/local_features/orb_feature.py
@@ -9,25 +9,47 @@ class ORBFeature(AbstractLocalFeature):
         self,
         resize_size: tuple[int, int],
         bovw_n_clusters_range: tuple[int, int],
-        n_keypoints: int,
-        patch_size: int,
+        n_features: int = 500,
+        scale_factor: float = 1.2,
+        n_levels: int = 8,
+        first_level: int = 0,
+        wta_k: int = 2,
+        patch_size: int = 31,
+        fast_threshold: int = 20,
     ) -> None:
         """Inits a ORBFeature instance. The underlying implementation relies on OpenCV's
-        implementation of the ORB feature.
+        implementation of the ORB feature. The parameter descriptions are taken from the
+        documentation of OpenCV. For more details, please refer to
+        https://docs.opencv.org/4.x/db/d95/classcv_1_1ORB.html
 
         :param resize_size: A 2-tuple of integers indicating the pixel width and height
             of the resized image.
         :param bovw_n_clusters_range: A 2-tuple of integers indicating the start
             (inclusive) and stop (exclusive) for the range of values to try for the
             number of clusters used for bag-of-visual-words.
-        :param n_keypoints: An integer indicating the maximum number of keypoints to
-            detect in an image.
-        :param patch_size: An integer indicating the value to use for the edgeThreshold
-            and patchSize parameters of OpenCV's ORB implementation.
+        :param n_features: An integer indicating the maximum number of features to
+            retain.
+        :param scale_factor: A float indicating the pyramid decimation ratio, greater
+            than 1.
+        :param n_levels: An integer indicating the number of pyramid levels.
+        :param first_level: An integer indicating the level of pyramid to put source
+            image to. Previous layers are filled with upscaled source image.
+        :param wta_k: An integer indicating the number of points that produce each
+            element of the oriented BRIEF descriptor.
+        :param patch_size: An integer indicating the size of the patch used by the
+            oriented BRIEF descriptor.
+        :param fast_threshold: An integer indicating the fast threshold
         """
         super().__init__(resize_size, bovw_n_clusters_range)
         self.orb: cv.ORB = cv.ORB_create(
-            nfeatures=n_keypoints, edgeThreshold=patch_size, patchSize=patch_size
+            nfeatures=n_features,
+            scaleFactor=scale_factor,
+            nlevels=n_levels,
+            edgeThreshold=patch_size,
+            firstLevel=first_level,
+            WTA_K=wta_k,
+            patchSize=patch_size,
+            fastthreshold=fast_threshold,
         )
 
     def read_image(self, image_path: str) -> np.ndarray:
