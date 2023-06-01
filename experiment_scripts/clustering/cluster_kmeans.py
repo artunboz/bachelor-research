f406cd7dd90f2ec09db1d21061fd7b928e6f4dea
Artun Boz <artunboz@outlook.com>
2023-06-01 21:06:07 +0200

    Add hog kmeans script

--- experiment_scripts/clustering/cluster_kmeans.py
+++ experiment_scripts/clustering/cluster_kmeans.py
@@ -5,9 +5,9 @@ from tqdm import tqdm
 from paths import DATA_DIR
 from src.clustering.kmeans_clustering import KMeansClustering
 
-n_clusters_space = [10, 11, 12, 13, 14]
+n_clusters_space = [12, 13, 14]
 
-root_folder = f"{DATA_DIR}/lbp"
+root_folder = f"{DATA_DIR}/hog"
 eval_folders = sorted(os.listdir(root_folder))
 
 for i, n_clusters in enumerate(n_clusters_space):
