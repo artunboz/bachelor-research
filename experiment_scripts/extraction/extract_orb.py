2ac5fba3cd128889585be8b87257ac8fd37902b7
Artun Boz <artunboz@outlook.com>
2023-06-02 17:05:42 +0200

    Remove 75 from n_components

--- experiment_scripts/extraction/extract_orb.py
+++ experiment_scripts/extraction/extract_orb.py
@@ -7,7 +7,7 @@ image_folder_path = f"{DATA_DIR}/extracted_images/face_images"
 
 resize_size_space = [(300, 300), (400, 400)]
 quantization_method_space = ["fisher", "bovw"]
-n_components_space_space = [[50], [75]]
+n_components_space_space = [[50]]
 n_features_space = [10]
 scale_factor_space = [1.2]  # default
 n_levels_space = [8]  # default
@@ -41,8 +41,6 @@ for i, (
         fast_threshold_space,
     )
 ):
-    if i < 3:
-        continue
     orb = ORBFeature(
         resize_size=resize_size,
         quantization_method=quantization_method,
