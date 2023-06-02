c084d19f91962c374605455636e178714d3fe218
Artun Boz <artunboz@outlook.com>
2023-06-02 13:22:58 +0200

    Lower n_features

--- experiment_scripts/extraction/extract_orb.py
+++ experiment_scripts/extraction/extract_orb.py
@@ -7,8 +7,8 @@ image_folder_path = f"{DATA_DIR}/extracted_images/face_images"
 
 resize_size_space = [(300, 300), (400, 400)]
 quantization_method_space = ["fisher", "bovw"]
-n_components_space_space = [[100], [200]]
-n_features_space = [20, 50]
+n_components_space_space = [[50], [100]]
+n_features_space = [10]
 scale_factor_space = [1.2]  # default
 n_levels_space = [8]  # default
 first_level_space = [0]  # default
