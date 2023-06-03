9e50081d22233018d5b051edbef25536f8479aa7
Artun Boz <artunboz@outlook.com>
2023-06-03 16:11:53 +0200

    save config

--- experiment_scripts/extraction/extract_hog.py
+++ experiment_scripts/extraction/extract_hog.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from tqdm.contrib.itertools import product
 
 from paths import DATA_DIR
@@ -10,6 +11,16 @@ orientations_space = [6, 9]
 pixels_per_cell_space = [(8, 8), (16, 16)]
 cells_per_block_space = [(2, 2), (3, 3)]
 block_norm_space = ["L1", "L1-sqrt", "L2", "L2-Hys"]
+
+configs_df = pd.DataFrame(
+    columns=[
+        "resize_size",
+        "orientations",
+        "pixels_per_cell",
+        "cells_per_block",
+        "block_norm",
+    ]
+)
 
 for i, (
     resize_size,
@@ -35,3 +46,13 @@ for i, (
     )
     hog.extract_features(image_folder_path=image_folder_path)
     hog.save_features(f"{DATA_DIR}/hog/run_{i}")
+
+    configs_df.loc[i] = {
+        "resize_size": resize_size,
+        "orientations": orientations,
+        "pixels_per_cell": pixels_per_cell,
+        "cells_per_block": cells_per_block,
+        "block_norm": block_norm,
+    }
+
+configs_df.to_csv(f"{DATA_DIR}/hog/configs.csv", index=False)
