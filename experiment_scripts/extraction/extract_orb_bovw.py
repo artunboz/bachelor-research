ff1f1ac4444a298002c31347c47f6702039ad078
Artun Boz <artunboz@outlook.com>
2023-06-04 03:14:53 +0200

    Add scripts

--- experiment_scripts/extraction/extract_orb_bovw.py
+++ experiment_scripts/extraction/extract_orb_bovw.py
@@ -6,15 +6,15 @@ from src.features.local_features.orb_feature import ORBFeature
 
 image_folder_path = f"{DATA_DIR}/extracted_images/face_images"
 
-resize_size_space = [(300, 300), (400, 400)]
-quantization_method_space = ["fisher", "bovw"]
-n_components_space_space = [[50]]
-n_features_space = [10]
+resize_size_space = [(64, 64), (128, 128), (256, 256)]
+quantization_method_space = ["bovw"]
+n_components_space_space = [[10], [20], [50]]
+n_features_space = [10, 20]
 scale_factor_space = [1.2]  # default
 n_levels_space = [8]  # default
 first_level_space = [0]  # default
 wta_k_space = [2]  # default
-patch_size_space = [20, 30, 40]
+patch_size_space = [31]  # default
 fast_threshold_space = [20]  # default
 
 configs_df = pd.DataFrame(
@@ -24,12 +24,12 @@ configs_df = pd.DataFrame(
         "quantization_method",
         "n_components_space",
         "n_features",
-        "scale_factor",
-        "n_levels",
-        "first_level",
-        "wta_k",
-        "patch_size",
-        "fast_threshold",
+        # "scale_factor",
+        # "n_levels",
+        # "first_level",
+        # "wta_k",
+        # "patch_size",
+        # "fast_threshold",
     ]
 )
 
@@ -71,7 +71,7 @@ for i, (
         fast_threshold=fast_threshold,
     )
     orb.extract_features(image_folder_path=image_folder_path)
-    orb.save_features(f"{DATA_DIR}/orb/run_{i}")
+    orb.save_features(f"{DATA_DIR}/orb_bovw/run_{i}")
 
     configs_df.loc[i] = {
         "name": f"run_{i}",
@@ -79,12 +79,12 @@ for i, (
         "quantization_method": quantization_method,
         "n_components_space": n_components_space,
         "n_features": n_features,
-        "scale_factor": scale_factor,
-        "n_levels": n_levels,
-        "first_level": first_level,
-        "wta_k": wta_k,
-        "patch_size": patch_size,
-        "fast_threshold": fast_threshold,
+        # "scale_factor": scale_factor,
+        # "n_levels": n_levels,
+        # "first_level": first_level,
+        # "wta_k": wta_k,
+        # "patch_size": patch_size,
+        # "fast_threshold": fast_threshold,
     }
 
-configs_df.to_csv(f"{DATA_DIR}/orb/configs.csv", index=False)
+configs_df.to_csv(f"{DATA_DIR}/orb_bovw/configs.csv", index=False)
