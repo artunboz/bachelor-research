0816a0e6d23e1618b22c0746622bd27c5ce5a46e
Artun Boz <artunboz@outlook.com>
2023-06-04 23:12:50 +0200

    Add get config script

---
+++ experiment_scripts/get_run_configs.py
@@ -0,0 +1,25 @@
+import json
+import os
+from argparse import ArgumentParser
+
+import pandas as pd
+
+from paths import DATA_DIR
+
+parser = ArgumentParser()
+parser.add_argument("feature")
+args = parser.parse_args()
+
+root_folder = f"{DATA_DIR}/{args.feature}"
+files = []
+for run in os.listdir(root_folder):
+    if run.startswith("run_"):
+        files.append(f"{root_folder}/{run}")
+
+combined_results = []
+for file in files:
+    with open(file, mode="r") as f:
+        combined_results.append(json.load(f)[0])
+
+configs_df = pd.DataFrame(combined_results)
+configs_df.to_csv(f"{root_folder}/results/configs.csv")
