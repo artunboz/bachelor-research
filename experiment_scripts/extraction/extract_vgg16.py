16d87e5e21e24cab5a16c39d06cb883a95ec2274
Artun Boz <artunboz@outlook.com>
2023-06-04 23:50:00 +0200

    Asdasd

--- experiment_scripts/extraction/extract_vgg16.py
+++ experiment_scripts/extraction/extract_vgg16.py
@@ -5,4 +5,4 @@ image_folder_path = f"{DATA_DIR}/extracted_images/face_images"
 
 vgg16 = VGG16Feature()
 vgg16.extract_features(image_folder_path=image_folder_path)
-vgg16.save_features(f"{DATA_DIR}/lbp/run_0")
+vgg16.save_features(f"{DATA_DIR}/vgg16/run_0")
