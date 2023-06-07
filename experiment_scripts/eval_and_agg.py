9309e16aedc1db4587f3e3adc313a569eb206599
Artun Boz <artunboz@outlook.com>
2023-06-07 14:09:46 +0200

    Update eval script

--- experiment_scripts/eval_and_agg.py
+++ experiment_scripts/eval_and_agg.py
@@ -13,7 +13,6 @@ parser.add_argument("--feature")
 parser.add_argument("--n-runs", type=int, dest="n_runs")
 args = parser.parse_args()
 
-clustering_type = "kmeans"
 root_folder = f"{DATA_DIR}/{args.feature}"
 eval_folders = sorted(os.listdir(root_folder))
 if "results" in eval_folders:
@@ -21,42 +20,43 @@ if "results" in eval_folders:
 if "configs.csv" in eval_folders:
     eval_folders.remove("configs.csv")
 
-for i in range(args.n_runs):
-    # Compute results.
-    for folder in tqdm(eval_folders, desc="Evaluated Folders"):
-        folder_path = f"{root_folder}/{folder}"
-        evaluator = Evaluator(
-            features_path=f"{folder_path}/features.npy",
-            cluster_labels_folder_path=f"{folder_path}/clustering/{clustering_type}/run_{i}",
-            image_names_path=f"{root_folder}/{folder}/image_names.pickle",
-            ground_truth_path=f"{DATA_DIR}/labelled_faces/clean_labels.csv",
-        )
-        evaluator.compute_metrics()
-        evaluator.save_metrics()
+clustering_type = "kmeans"
 
-    all_results_df = pd.DataFrame(
-        columns=[
-            "name",
-            "image_count",
-            "silhouette",
-            "davies_bouldin",
-            "precision",
-            "recall",
-            "f1",
-        ]
+# Compute results.
+for folder in tqdm(eval_folders, desc="Evaluated Folders"):
+    folder_path = f"{root_folder}/{folder}"
+    evaluator = Evaluator(
+        features_path=f"{folder_path}/features.npy",
+        cluster_labels_folder_path=f"{folder_path}/clustering/{clustering_type}",
+        image_names_path=f"{root_folder}/{folder}/image_names.pickle",
+        ground_truth_path=f"{DATA_DIR}/labelled_faces/clean_labels.csv",
     )
-    # Aggregate results.
-    for folder in eval_folders:
-        folder_path = f"{root_folder}/{folder}"
-        with open(
-            f"{folder_path}/clustering/{clustering_type}/run_{i}/metrics.json", mode="r"
-        ) as f:
-            d = json.load(f)
-            d["name"] = folder
-            d_df = pd.DataFrame([d])
-            all_results_df = pd.concat([all_results_df, d_df], ignore_index=True)
+    evaluator.compute_metrics()
+    evaluator.save_metrics()
 
-    results_path = f"{root_folder}/results"
-    if not os.path.isdir(results_path):
-        os.mkdir(results_path)
-    all_results_df.to_csv(f"{results_path}/run_{i}_results.csv", index=False)
+all_results_df = pd.DataFrame(
+    columns=[
+        "name",
+        "image_count",
+        "silhouette",
+        "davies_bouldin",
+        "precision",
+        "recall",
+        "f1",
+    ]
+)
+# Aggregate results.
+for folder in eval_folders:
+    folder_path = f"{root_folder}/{folder}"
+    with open(
+        f"{folder_path}/clustering/{clustering_type}/metrics.json", mode="r"
+    ) as f:
+        d = json.load(f)
+        d["name"] = folder
+        d_df = pd.DataFrame([d])
+        all_results_df = pd.concat([all_results_df, d_df], ignore_index=True)
+
+results_path = f"{root_folder}/results"
+if not os.path.isdir(results_path):
+    os.mkdir(results_path)
+all_results_df.to_csv(f"{results_path}/kmeans_results.csv", index=False)
