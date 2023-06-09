67b22bf712a06c587461542831f3a494823f7b5b
Artun Boz <artunboz@outlook.com>
2023-06-09 16:10:01 +0200

    asdasd

--- experiment_scripts/reduce_cluster_eval.py
+++ experiment_scripts/reduce_cluster_eval.py
@@ -5,15 +5,16 @@ from paths import SCRIPTS_DIR
 
 parser = ArgumentParser()
 parser.add_argument("--reduction")
-parser.add_argument("--feature-path", dest="feature_path")
+parser.add_argument("--feature-path-original", dest="feature_path_original")
+parser.add_argument("--feature-path-reduced", dest="feature_path_reduced")
 args = parser.parse_args()
 
 os.system(
-    f"python {SCRIPTS_DIR}/reduction/reduce_{args.reduction}.py --feature-path {args.feature_path}"
+    f"python {SCRIPTS_DIR}/reduction/reduce_{args.reduction}.py --feature-path {args.feature_path_original}"
 )
 os.system(
-    f"python {SCRIPTS_DIR}/clustering/cluster_kmeans.py --feature-path {args.feature_path}"
+    f"python {SCRIPTS_DIR}/clustering/cluster_kmeans.py --feature-path {args.feature_path_reduced}"
 )
 os.system(
-    f"python {SCRIPTS_DIR}/eval_and_agg.py --feature-path {args.feature_path} --n-runs 1"
+    f"python {SCRIPTS_DIR}/eval_and_agg.py --feature-path {args.feature_path_reduced} --n-runs 1"
 )
